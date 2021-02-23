a544cfb4a648b6d1ad9d2c1c7838d109824ceade
sbrenko <sbrenko@solvd.com>
2021-02-23 11:41:05 +0300

    minor refactoring

--- src/test/java/testng/TestConfigurationCollector.java
+++ src/test/java/testng/TestConfigurationCollector.java
@@ -1,15 +1,20 @@
 package testng;
 
 import com.zebrunner.agent.core.registrar.CurrentTestRun;
+import com.zebrunner.agent.core.registrar.TestSessionRegistrar;
+import com.zebrunner.agent.core.registrar.descriptor.SessionCloseDescriptor;
+import com.zebrunner.agent.core.registrar.descriptor.SessionStartDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
 import java.util.TreeSet;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class TestConfigurationCollector {
@@ -28,11 +33,17 @@ public class TestConfigurationCollector {
         Map<String, String> envProperties = System.getenv();
         TreeSet<Object> keys = new TreeSet<>(envProperties.keySet());
 
+        SessionStartDescriptor startDescriptor = getStartDescriptor("Chrome", "100", "iOS", "18.2", "Space Rocket");
+        TestSessionRegistrar.getInstance().registerStart(startDescriptor);
+
         log.info("Sleeping for 5 seconds...");
         TimeUnit.SECONDS.sleep(5);
 
         log.info("All available Environment Properties");
         keys.forEach(key -> log.info("'{}' : '{}'", key, envProperties.get(key)));
+
+        SessionCloseDescriptor closeDescriptor = new SessionCloseDescriptor(startDescriptor.getSessionId());
+        TestSessionRegistrar.getInstance().registerClose(closeDescriptor);
     }
 
     @Test(testName = "System Properties")
@@ -45,6 +56,25 @@ public class TestConfigurationCollector {
 
         log.info("All available System Properties");
         keys.forEach(key -> log.info("'{}' : '{}'", key, systemProperties.get(key)));
+    }
+
+    private SessionStartDescriptor getStartDescriptor(String browser, String browserVersion,
+                                                      String platform, String platformVersion,
+                                                      String device) {
+        HashMap<String, Object> capabilities = new HashMap<>();
+        capabilities.put("browserName", browser);
+        capabilities.put("browserVersion", browserVersion);
+        capabilities.put("platformName", platform);
+        capabilities.put("platformVersion", platformVersion);
+        capabilities.put("deviceModel", device);
+
+        HashMap<String, Object> desiredCapabilities = new HashMap<>();
+
+        return new SessionStartDescriptor(
+                UUID.randomUUID().toString(),
+                capabilities,
+                desiredCapabilities
+        );
     }
 
 }
